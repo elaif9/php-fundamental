1814f91acd8f574e6930e9e0d1b483179d21a8aa
Alif Nur Rachman <alifnurrachman907@gmail.com>
2020-05-20 05:07:10 +0700

    4 - Int & Float, Revisi 3 - String

--- 3_string.php
+++ 3_string.php
@@ -8,13 +8,17 @@
  *      sebagai satu kalimat / tulisan utuh
  * Tanda string adalah single / double quote --> '' dan ""
  * 
- * Gunakan tanda --> \
+ * Gunakan tanda garis miring --> \
  *      Jika ingin menampilkan simbol atau tanda huruf ketika echo file php
+ * 
+ * Gunakan tanda titik --> .
+ *      Jika ingin menggabungkan tanda 
  */
 
 $var_string = "tipe data string";
+$string = "tipe data string";
 
 echo "ini adalah $var_string <br>";
-echo "sedangkan ini adalah variabel \$var_string";
+echo "sedangkan ini adalah variabel \$var_string dengan isi" . $string;
 
 ?>
---
+++ 4_integer_float.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Program PHP keempat = integer & float  
+ * Mengetahui tipe data integer dan float 
+ * 
+ * Integer = data bilangan bulat (desimal), hexa, octa, binary, atau tanda matematis + dan -
+ * Float   = data bilangan dengan koma 
+ *      untuk float, jika ingin menggunakan koma, gunakan tanda titik --> .
+ * 
+ * */
+
+$integer = 1000;
+$float = 500.4231;
+$jumlahinteger = $integer + $float;
+
+echo "integer pertama adalah $integer <br>";
+echo "jika \$integer = 1000 dan \$float = 500.4231, maka penjumlahannya " . $jumlahinteger;
+
+?>
