ce37c0465e1c1a11b331841a1d5886d7a8c8a3e4
Alif Nur Rachman <alifnurrachman907@gmail.com>
2020-05-19 21:30:28 +0700

    2 - Variable, dan revisi 1 - Hello

--- 1_hello.php
+++ 1_hello.php
@@ -1,4 +1,4 @@
-<?php 
+<?php // Struktur file php harus diawali dengan tag <?php
 
 /**
  * Program PHP Pertama = echo & print
@@ -9,4 +9,5 @@
 
 echo "this is php fundamental: ";
 print "welcome aboard!"
-?>
+
+// dan ini adalah tag penutup program file php ?>
---
+++ 2_variable.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * Program PHP Pertama = echo & print
+ * Umtuk mengeluarkan output dari program file ke browser
+ * Bisa menggunakan perintah echo atau print
+ * 
+ */
+
+    // Ini adalah variable
+    $x = "try variable";
+
+    // mencoba print isi dari variable
+    echo $x;
+    echo "<br> this is " . $x . " too";
+
+?>
