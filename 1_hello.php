96137d45ca3e386cf649be6ecdc89bb61448d5df
Alif Nur Rachman <alifnurrachman907@gmail.com>
2020-05-19 21:25:13 +0700

    1 - Hello

---
+++ 1_hello.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * Program PHP Pertama = echo & print
+ * Umtuk mengeluarkan output dari program file ke browser
+ * Bisa menggunakan perintah echo atau print
+ * 
+ */
+
+echo "this is php fundamental: ";
+print "welcome aboard!"
+?>
