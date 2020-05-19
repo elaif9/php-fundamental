67f15860d8fd919ada9255a5b35201b1bbd1d83d
Alif Nur Rachman <alifnurrachman907@gmail.com>
2020-05-20 04:06:17 +0700

    3 - String, Revisi 2 - Variable

--- 2_variable.php
+++ 2_variable.php
@@ -1,16 +1,24 @@
 <?php 
 
 /**
- * Program PHP Pertama = echo & print
+ * Program PHP kedua = variable
  * Umtuk mengeluarkan output dari program file ke browser
- * Bisa menggunakan perintah echo atau print
+ *      dengan variabel
  * 
  */
 
     /**
      * Ini adalah variable
+     * Variabel berisikan inisial dan isi dari variabel
      * Variabel tidak boleh diawali atau hanya diisi dengan angka
-     * Boleh diawali dengan underscore
+     * Tanda baca yang boleh digunakan pada variabel adalah underscore
+     *      Penggunaan underscore bisa diawal, ditengah, atau di akhir
+     *      Contoh --> $_x1
+     *                 $x_1
+     *                 $x1_
+     * 
+     * contohnya $x, kita akan gunakan $x sebagai inisialisasi variabel
+     * sedangakan $x bisa kita isi dengan data
      */
 
     $x = "try variable";
---
+++ 3_string.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Program PHP ketiga = string  
+ * Mengetahui tipe data string
+ * 
+ * Data string adalah isi dari variabel yang dibaca
+ *      sebagai satu kalimat / tulisan utuh
+ * Tanda string adalah single / double quote --> '' dan ""
+ * 
+ * Gunakan tanda --> \
+ *      Jika ingin menampilkan simbol atau tanda huruf ketika echo file php
+ */
+
+$var_string = "tipe data string";
+
+echo "ini adalah $var_string <br>";
+echo "sedangkan ini adalah variabel \$var_string";
+
+?>
