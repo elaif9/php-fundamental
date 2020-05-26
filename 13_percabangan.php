450a4b3c0e93805ebf7c5b6954ef86b9d450c5e5
Alif Nur Rachman <alifnurrachman907@gmail.com>
2020-05-26 20:46:20 +0700

    13 - Percabangan

---
+++ 13_percabangan.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Program PHP ketigabelas = percabangan
+ * Mengetahui percabgan pada PHP 
+ * 
+ * if-else --> if-else adalah metode percabangan jika nilai memenuhi syarat, maka akan menghasilkan
+ *              sesuatu, sedangkan jika tidak memenuhi syarat, maka akan masuk ke kondisi yang lain
+ * 
+ * */
+
+ $sembilan = 9;
+
+ if($sembilan == 9){
+     echo "angkanya adalah $sembilan";
+ } else {
+     echo "nilainya bukan $sembilan";
+ }
+?>
